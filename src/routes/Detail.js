29e28730e025a48844d306fa23addd12bd28d614
sangjun <juntech0421@github.com>
2021-04-12 20:33:53 +0900

    grapql query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,2 +1,25 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => "Detail";
+export default () => {
+  const { id } = useParams();
+  const {loading, data} = useQuery(GET_MOVIE, {
+      variables: {id}
+  })
+  console.log(loading, data)
+  return <h1>Detail</h1>;
+};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,19 @@
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => "Home";
+export default () => {
+  const { loading,error,data } = useQuery(GET_MOVIES);
+  console.log(loading,error,data)
+  return <h1>Home</h1>;
+};
